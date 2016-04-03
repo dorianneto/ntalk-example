fb193f3fb1dfbd2a083195a00aba58f40642c431
Dorian Neto <doriansampaioneto@gmail.com>
2016-04-03 17:09:36 -0300

    Inclusão de módulo express-load e estrutura MVC

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require('express')
-  , routes = require('./routes');;
-var app = express();
+  , load = require('express-load')
+	, app = express();
 
 // Configurações
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.set(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// Rotas
-app.get('/', routes.index);
-app.get('/usuarios', routes.user.index);
+load('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
 
 app.listen(3000, function() {
   console.log('Ntalk Example online!');
