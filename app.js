0ff7b7f4b3e2efed5a7f4a62ef37d78faa9118e7
Dorian Neto <doriansampaioneto@gmail.com>
2016-04-14 00:36:24 -0300

    Inclusão de bundles de session e cookie e login/logout básico

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express')
   , load = require('express-load')
+  , cookieParser = require('cookie-parser')
+  , session = require('express-session')
+  , bodyParser = require('body-parser')
 	, app = express();
 
 // Configurações
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(session());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 load('models')
