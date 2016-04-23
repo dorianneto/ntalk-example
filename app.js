509cc6dc816228354c2f3900be20bf6d9f24c1d8
Dorian Neto <doriansampaioneto@gmail.com>
2016-04-23 17:16:50 -0300

    Inclusão de módulo pra métodos rest e middleware paratratamento de erros

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express')
-  , load = require('express-load')
-  , cookieParser = require('cookie-parser')
-  , session = require('express-session')
-  , bodyParser = require('body-parser')
-	, app = express();
+var express      	 = require('express')
+	, load           = require('express-load')
+	, cookieParser   = require('cookie-parser')
+	, session        = require('express-session')
+	, bodyParser     = require('body-parser')
+	, methodOverride = require('method-override')
+	, app            = express();
 
 // Configurações
 app.set('views', __dirname + '/views');
@@ -12,12 +13,21 @@ app.use(cookieParser('ntalk'));
 app.use(session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 
 load('models')
 	.then('controllers')
 	.then('routes')
 	.into(app);
+
+// more middleware (executes after routes)
+app.use(function(req, res, next) {});
+
+// error handling middleware
+app.use(function(err, req, res, next) {
+	console.log(err);
+});
 
 app.listen(3000, function() {
   console.log('Ntalk Example online!');
