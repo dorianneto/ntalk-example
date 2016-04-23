bf69be78fc6bb71b21ee9539f14498ac0faf4f3d
Dorian Neto <doriansampaioneto@gmail.com>
2016-04-23 17:18:15 -0300

    Métodos CRUD e roteamento REST (contatos)

--- controllers/contato.js
+++ controllers/contato.js
@@ -1,9 +1,63 @@
 module.exports = function(app) {
 	var ContatoController = {
 		index: function(req, res) {
-			var parameters = { usuario: req.session.usuario };
+			var usuario = req.session.usuario
+				, contatos = usuario.contatos
+				, parameters = {
+					usuario: usuario,
+					contatos: contatos
+				};
 
 			res.render('contato/index', parameters);
+		},
+		create: function(req, res) {
+			var usuario = req.session.usuario
+				, contato = req.body.contato;
+
+			// Salva registro
+			usuario.contatos.push(contato);
+
+			res.redirect('/contatos');
+		},
+		show: function(req, res) {
+			var id = req.params.id
+				, contato = req.session.usuario.contatos[id]
+				, parameters = {
+					id: id,
+					contato: contato
+				};
+
+			res.render('contato/show', parameters);
+		},
+		edit: function(req, res) {
+			var id = req.params.id
+				, usuario = req.session.usuario
+				, contato = usuario.contatos[id]
+				, parameters = {
+					id: id,
+					usuario: usuario,
+					contato: contato
+				};
+
+			res.render('contato/edit', parameters);
+		},
+		update: function(req, res) {
+			var id = req.params.id
+				, usuario = req.session.usuario
+				, contato = req.body.contato;
+
+			// Atualiza registro
+			usuario.contatos[id] = contato;
+
+			res.render('contato/index');
+		},
+		destroy: function(req, res) {
+			var id = req.params.id
+				, usuario = req.session.usuario;
+
+			usuario.contatos.splice(id, 1);
+
+			res.render('contato/index');
 		}
 	};
 
--- routes/contato.js
+++ routes/contato.js
@@ -1,4 +1,10 @@
 module.exports = function(app) {
 	var contato = app.controllers.contato;
+
 	app.get('/contatos', contato.index);
+	app.get('/contatos/:id', contato.show);
+	app.post('/contatos', contato.create);
+	app.get('/contatos/:id/editar', contato.edit);
+	app.put('/contatos/:id', contato.update);
+	app.delete('/contatos/:id', contato.destroy);
 }
