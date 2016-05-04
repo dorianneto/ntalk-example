3c550db1cc3ea0391e30d29b82f53abaf12c29e5
Dorian Neto <doriansampaioneto@gmail.com>
2016-05-03 23:03:49 -0300

    Criação de middleware para autenticação

--- routes/contato.js
+++ routes/contato.js
@@ -1,10 +1,11 @@
 module.exports = function(app) {
-	var contato = app.controllers.contato;
+	var contato    = app.controllers.contato
+	, autenticador = require('../middleware/autenticador');
 
-	app.get('/contatos', contato.index);
-	app.get('/contatos/:id', contato.show);
-	app.post('/contatos', contato.create);
-	app.get('/contatos/:id/editar', contato.edit);
-	app.put('/contatos/:id', contato.update);
-	app.delete('/contatos/:id', contato.destroy);
+	app.get('/contatos', autenticador, contato.index);
+	app.get('/contatos/:id', autenticador, contato.show);
+	app.post('/contatos', autenticador, contato.create);
+	app.get('/contatos/:id/editar', autenticador, contato.edit);
+	app.put('/contatos/:id', autenticador, contato.update);
+	app.delete('/contatos/:id', autenticador, contato.destroy);
 }
