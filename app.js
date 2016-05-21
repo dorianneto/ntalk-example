fc7f56d9da045e44347116462c4f7b541e279f31
Dorian Neto <doriansampaioneto@gmail.com>
2016-05-21 11:29:23 -0300

    chat simples entre contatos utilizando socket.io

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express      	 = require('express')
+	, app            = express()
 	, load           = require('express-load')
 	, cookieParser   = require('cookie-parser')
 	, session        = require('express-session')
 	, bodyParser     = require('body-parser')
 	, methodOverride = require('method-override')
-	, error 		 = require('./middleware/error')
-	, app            = express();
+	, error 		 		 = require('./middleware/error')
+	, server 		 		 = require('http').createServer(app)
+	, io 			 			 = require('socket.io').listen(server);
 
 // Configurações
 app.set('views', __dirname + '/views');
@@ -28,6 +30,14 @@ app.use(error.notFound);
 // error handling middleware
 app.use(error.serverError);
 
-app.listen(3000, function() {
+io.sockets.on('connection', function(client) {
+	client.on('send-server', function(data) {
+		var msg = '<strong>' + data.nome + ':</strong> ' + data.msg + '<br>';
+		client.emit('send-client', msg);
+		client.broadcast.emit('send-client', msg);
+	})
+});
+
+server.listen(3000, function() {
   console.log('Ntalk Example online!');
 });
